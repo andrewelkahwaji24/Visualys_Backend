29b390d3daa32b344b1f940bd0d24defaaf1aaf1
Kevin Roussety <kevinroussety1@gmail.com>
2025-10-13 09:27:28 +0200

    Enabling connection to mongoDB

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
-const uri = "mongodb+srv://admin:aM7bAvNBfhV2vDVd@iliji.o2sjla9.mongodb.net/?retryWrites=true&w=majority&appName=Iliji";
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@iliji.o2sjla9.mongodb.net/?retryWrites=true&w=majority&appName=Iliji`;
 const connectDB = async () => {
   try {
     await mongoose.connect(uri, {
