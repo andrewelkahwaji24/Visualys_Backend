c3c7987dd9a3ec4cd637e0afc9ef442629ac624b
Andrew El Kahwaji <158772084+andrewelkahwaji24@users.noreply.github.com>
2025-10-14 07:59:29 +0200

    MAJ sur les modeles

--- models/projet.js
+++ models/projet.js
@@ -1,24 +1,20 @@
-// In /models/projetSchema.js
 
 const mongoose = require("mongoose");
 const projetSchema = require ("./projetSchema");
 
 
-// Get all projects
 projetSchema.statics.getAllProjets = async function (userId) {
     if (!userId) throw new Error("User ID is required");
     return this.find({ user: userId });
 };
 
 
-// Get project by ID
 projetSchema.statics.getProjetById = async function (projetId) {
     const projet = await this.findById(projetId);
     if (!projet) throw new Error("Projet not found");
     return projet;
 };
 
-// Create a new project
 projetSchema.methods.createProjet = async function (data) {
     try {
         const projet = new this(data);
