e0d4c7edd9abb3e5d9e88576354957ebd3d50ee7
Kevin Roussety <kevinroussety1@gmail.com>
2025-10-14 13:14:26 +0200

    [ONGOING] Adding mailer for reseting password

--- routes/mailRoutes.js
+++ routes/mailRoutes.js
@@ -6,8 +6,10 @@ router.post('/send/:email', async (req, res) => {
     try {
         const { to, subject, text } = req.body;
         const message = {
-            from: "Visualys <no-reply@visualys.fr>"
-            to
+            from: "Visualys <no-reply@visualys.fr>",
+            to: "" + req.params.email,
+            subject: subject || "No Subject",
+            text: text || "No Content"
         }
     } catch (error) {
         
