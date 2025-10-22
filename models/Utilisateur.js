26a0ae7fbf0acd8d6822a918e8d58da9d2560617
Andrew El Kahwaji <158772084+andrewelkahwaji24@users.noreply.github.com>
2025-10-22 10:07:40 +0200

    Count users

--- models/Utilisateur.js
+++ models/Utilisateur.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcrypt");
 const crypto = require("crypto");
 const userSchema = require("./UtilisateurSchema");
 
+
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   const salt = await bcrypt.genSalt(10);
